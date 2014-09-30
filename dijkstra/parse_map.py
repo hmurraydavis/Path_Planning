ccba8013465576305c8f5c919f87bf11195aee65
Halie Murray-Davis <hmurraydavis@yahoo.com>
2014-09-30 13:23:11 -0400

    Iterating through 2D list works

--- dijkstra/parse_map.py
+++ dijkstra/parse_map.py
@@ -1,14 +1,10 @@
 #PARSE THE CSV, BINARY (0 OR 1), *.TXT MAP FILE:
 
+import csv
+
 map_path='/home/comprobo/Path_Planning/dijkstra/map1ex.txt'
 
 #map = open(map_path, 'r')
-
-#maptxt= map.read()
-
-#print type(maptxt)
-
-import csv
 
 with open(map_path, 'Ur') as f:
     data = list(tuple(rec) for rec in csv.reader(f, delimiter=','))
@@ -16,8 +12,10 @@ with open(map_path, 'Ur') as f:
 print 'type is: ', type(int(data[1][0]))
 
 for x in data:
-    for y in len(x):
-        print x[y]
+    print 'x is: ',len(x)
+    curent_row=x
+    for y in x:
+        print 'hi'
 #    print data[x]
 
 
