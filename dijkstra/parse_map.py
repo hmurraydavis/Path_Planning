39237a36f13fa39c42bcaeeb744cfca3928b361c
Halie Murray-Davis <hmurraydavis@yahoo.com>
2014-09-30 14:05:36 -0400

    finds neighbors

--- dijkstra/parse_map.py
+++ dijkstra/parse_map.py
@@ -6,17 +6,23 @@ map_path='/home/comprobo/Path_Planning/dijkstra/map1ex.txt'
 
 #map = open(map_path, 'r')
 
+#Read in from CSV file:
 with open(map_path, 'Ur') as f:
     data = list(tuple(rec) for rec in csv.reader(f, delimiter=','))
 
-print 'type is: ', type(int(data[1][0]))
-
-for x in data:
-    print 'x is: ',len(x)
-    curent_row=x
-    for y in x:
-        print 'hi'
-#    print data[x]
-
-
-#map.close()
+#parse the resulting thin:g
+for x, x_items in enumerate(data):
+    for y, location in enumerate(x_items):
+        neighbors=[]
+        potential_neighbors=[(x+1,y),
+			     (x-1,y),
+			     (x,y+1),
+			     (x,y-1),
+			     (x+1,y+1),
+			     (x-1,y-1),
+			     (x+1,y-1),
+			     (x-1,y+1)]
+	print potential_neighbors
+        #for neighbor in potential_neighbors:
+ 
+#	map={tuple(x,y):
