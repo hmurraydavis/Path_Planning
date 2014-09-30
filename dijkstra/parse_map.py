8d4e3b639aa4843b69acaa873517e386a627e424
Halie Murray-Davis <hmurraydavis@yahoo.com>
2014-09-30 04:02:02 -0400

    Happy with CSV now

--- dijkstra/parse_map.py
+++ dijkstra/parse_map.py
@@ -2,6 +2,20 @@
 
 map_path='/home/comprobo/Path_Planning/dijkstra/map1ex.txt'
 
-map = open(map_path, 'r')
+#map = open(map_path, 'r')
 
-print map.read()
+#maptxt= map.read()
+
+#print type(maptxt)
+
+import csv
+
+with open(map_path, 'Ur') as f:
+    data = list(tuple(rec) for rec in csv.reader(f, delimiter=','))
+
+print data
+print type(data)
+print 'tp 1=: ', data[1][0]
+print type(int(data[1][0]))
+
+#map.close()
