a2245bcc812c681f0b72f5a0df894692cb35740f
Halie Murray-Davis <hmurraydavis@yahoo.com>
2014-10-01 17:27:47 -0400

    I like functions. You should too.

--- dijkstra/parse_map.py
+++ dijkstra/parse_map.py
@@ -21,32 +21,34 @@ def distance_between_neighbors(pt1,pt2):
        OUTPUT: float which is the distance between the two, given points'''
     return ( (pt2[1]-pt1[1])**2 + (pt2[0]-pt1[0])**2 )**.5
 
-data = read_csv('map2ex.txt')
 
-space={} #initialize the dictionary, representing thetraverable space of nodes
+def make_space_dict():
+	data = read_csv('map2ex.txt')
 
-#parse the resulting list of tuples of ints (effectively a 2d array)
-for y, x_items in enumerate(data):
-    for x, map_value_for_square in enumerate(x_items):
-        if (map_value_for_square>0): #1 is traverseable, 0 is not
-            neighbors={}
-            potential_neighbors=[(x+1,y),
-			         (x-1,y),
-			         (x,y+1),
-			         (x,y-1),
-			         (x+1,y+1),
-			         (x-1,y-1),
-			         (x+1,y-1),
-			         (x-1,y+1)]
-            for neighbor in potential_neighbors:
-                xn, yn = neighbor
-                if ((xn>=0) and (yn>=0) 
-                     and (xn<len(x_items)) and (yn<len(data))): #check if it's in bounds of the map
-                     if data[yn][xn]>0: #check if it's traversable. Data is read in backwards, so data[yn][xn] is correct.
-                         neighbors[(xn,yn)]=distance_between_neighbors((x,y),(xn,yn))
-                         
-            #construct dictionary of point and its neighbors!
-	    space[(x,y)] = neighbors
+	space={} #initialize the dictionary, representing thetraverable space of nodes
 
+	#parse the resulting list of tuples of ints (effectively a 2d array)
+	for y, x_items in enumerate(data):
+	    for x, map_value_for_square in enumerate(x_items):
+		if (map_value_for_square>0): #1 is traverseable, 0 is not
+		    neighbors={}
+		    potential_neighbors=[(x+1,y),
+					 (x-1,y),
+					 (x,y+1),
+					 (x,y-1),
+					 (x+1,y+1),
+					 (x-1,y-1),
+					 (x+1,y-1),
+					 (x-1,y+1)]
+		    for neighbor in potential_neighbors:
+			xn, yn = neighbor
+			if ((xn>=0) and (yn>=0) 
+			     and (xn<len(x_items)) and (yn<len(data))): #check if it's in bounds of the map
+			     if data[yn][xn]>0: #check if it's traversable. Data is read in backwards, so data[yn][xn] is correct.
+				 neighbors[(xn,yn)]=distance_between_neighbors((x,y),(xn,yn))
+				 
+		    #construct dictionary of point and its neighbors!
+		    space[(x,y)] = neighbors
+	return space
 
-
+print 'space is: ',make_space_dict()
