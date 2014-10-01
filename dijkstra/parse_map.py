7105dec27cb418d2e0a0e4f27771a3082d00182f
Halie Murray-Davis <hmurraydavis@yahoo.com>
2014-10-01 12:30:31 -0400

    X and Y correct in first section.

--- dijkstra/parse_map.py
+++ dijkstra/parse_map.py
@@ -2,7 +2,7 @@
 
 import csv
 
-map_path='/home/comprobo/Path_Planning/dijkstra/map1ex.txt'
+map_path='/home/comprobo/Path_Planning/dijkstra/map2ex.txt'
 
 #map = open(map_path, 'r')
 
@@ -11,8 +11,12 @@ with open(map_path, 'Ur') as f:
     data = list(tuple(rec) for rec in csv.reader(f, delimiter=','))
 
 #parse the resulting thing
-for x, x_items in enumerate(data):
-    for y, location in enumerate(x_items):
+for y, x_items in enumerate(data):
+    for x, location in enumerate(x_items):
+        print 'loc: ',location
+        print 'x: ', x
+        print 'x items: ', x_items
+        print 'y: ',y,'\n'
         neighbors=[]
         potential_neighbors=[(x+1,y),
 			     (x-1,y),
@@ -23,10 +27,12 @@ for x, x_items in enumerate(data):
 			     (x+1,y-1),
 			     (x-1,y+1)]
         for neighbor in potential_neighbors:
-            yn, xn=neighbor #should be: xn,yn
+            xn, yn = neighbor
             if ((xn>=0) and (yn>=0) and (yn<len(x_items)) and (xn<len(data))):
-		print (xn, yn)
+		#print (xn, yn), data[xn][yn]
                 if (data[xn][yn]>0): #1 is traversable, 0 is not
                     neighbors.append((xn,yn))
-            print 'neighbors of (',x,',',y,') are: ',neighbors
+            #print 'neighbors of (',x,',',y,') are: ',neighbors
 #	map={tuple(x,y):
+
+print data
