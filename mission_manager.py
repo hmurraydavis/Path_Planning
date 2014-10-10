413dae146b5c21e034d3cec5d9159fb4b618d18c
Halie Murray-Davis <hmurraydavis@yahoo.com>
2014-10-10 15:06:39 -0400

    Took out a lot of the bloat from original writing

--- mission_manager.py
+++ mission_manager.py
@@ -8,62 +8,24 @@ def distBtwnPts(pt1, pt2):
 	return ( (pt2[1]-pt1[1])**2 + (pt2[0]-pt1[0])**2  )**.5 #distance between 2 points formula
 
 def BuildList():
-	
-	number_of_modes=int(raw_input('How many modes would you like to have in your mission?'))
-	
+	number_of_modes=int(raw_input('How many points would you like to have in your mission?'))
 	mission=[]
 	
 	for mode in range (number_of_modes):
-		mode=raw_input('What mode would you like to use here? choces: "line follow," "obstacle avoid," "go to point," "maintain heading" ')
-		if mode=='line follow':
-			pt1=raw_input('GPS Coordinates of first point (in brackets:[])? ')
-			pt2=raw_input('GPS Coordinates of second point (in brackets:[])? ')
-			mission.append(['line follow',pt1,pt2])
-			
-		if mode=='obstacle avoid':
-			timeT=raw_input('How long would you like it to avoid obstacles on its own?')
-			mission.append(['obstacle avoid',timeT])
-			
 		if mode=='go to point':
-			pt=raw_input('GPS coordinates of point (in brackets:[])? ')
+			pt=raw_input('GPS coordinates of point (in parenthesis:())? ')
 			mission.append(['go to point',pt])
-			
-		if mode=='maintain heading':
-			heading=raw_input('Desired global heading (in degrees)?')
-			timeT=timeT=raw_input('How long would you like it to maintain this heading?')
-			mission.append(['maintain heading',heading,timeT])
-			
 	print 'Mission is:', mission
-	
-	#TODO: make the mission info not be a string ex: [['linefollow'], [2,3],[4,5]] all 1 string == sad
 	
 def missionMannager(mission): #mission is a list of dictionaries
 	print mission
 	for mis in mission:
 		print 'mission current ',mis
-		if mis['mode']=='line follow':
-			pt1 = mis['pt1']
-			print 'pt1 is:', pt1
-			pt2 = mis['pt2']
-			#TODO: Give it the robot's current heading:
-			currentPos=45
-			distToPt=distBtwnPts(currentPos, mis['pt2'])
-			print 'Dist between pts: ',distToPt
-			while distToPt>=0.00002:
-				forebrain.followLine(pt1, pt2) #followLine(self,posCurrent,linstart,posDesired
-				time.sleep(.25)
-		elif mis['mode']=='obstacle avoid':
-			for t in range (0,2*mis['time']):
-				forebrain.obsAvoid()
-		elif mis['mode']=='go to point':
+		if mis['mode']=='go to point':
 			#TODO:Give it the robot's current position
 			currentPos=(2,3)
 			while distBtwnPts(currentPos, mis['pt']) >= 0.00002:
 				forebrain.goToPoint(mis['pt'])
-				time.sleep(.25)
-		elif mis['mode']=='maintain heading':
-			for t in range (0,4*int(mis['time'])):
-				forebrain.mtnHeading(mis['heading'])
 				time.sleep(.25)
 missionT=[
 	{'mode':'Line follow',
@@ -77,4 +39,8 @@ missionT=[
 	'heading':70,
 	'time':20}
 ]
+
+mis=[
+
+    ]
 missionMannager(missionT)
