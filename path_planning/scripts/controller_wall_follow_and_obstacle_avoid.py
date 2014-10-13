871c45d9c718d712ad01840797f156c13a453c4c
jaazpa3 <jaazpa3@gmail.com>
2014-10-13 16:03:14 -0400

    refactoring code

--- path_planning/scripts/controller_wall_follow_and_obstacle_avoid.py
+++ path_planning/scripts/controller_wall_follow_and_obstacle_avoid.py
@@ -1,40 +1,6 @@
 #!/usr/bin/env python
-# Software License Agreement (BSD License)
-#
-# Copyright (c) 2008, Willow Garage, Inc.
-# All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions
-# are met:
-#
-#  * Redistributions of source code must retain the above copyright
-#    notice, this list of conditions and the following disclaimer.
-#  * Redistributions in binary form must reproduce the above
-#    copyright notice, this list of conditions and the following
-#    disclaimer in the documentation and/or other materials provided
-#    with the distribution.
-#  * Neither the name of Willow Garage, Inc. nor the names of its
-#    contributors may be used to endorse or promote products derived
-#    from this software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
-# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-# POSSIBILITY OF SUCH DAMAGE.
-#
-# Revision $Id$
-
-## Simple talker demo that published std_msgs/Strings messages
-## to the 'chatter' topic
+# Path_Planning Team
+# Jasper Chen
 
 import tf
 import math
@@ -48,14 +14,18 @@ dist_target = 0
 index = 0
 speed = 0
 
-list_of_points = [(1,1)]
+list_of_way_points = [(1,1)]
 
-def scan_received(msg):
-    """ Processes data from particle"""
+def calculate_heading_speed(msg):
+    """ Subscribes to /particle node, which is the mean of all particles produced by the particle filter.
+    Processes data from /particle node of type PoseStamped, which gives an estimated current position and direction. Outputs constants
+    speed and angle, which are parameters used in get_to_waypoint() and published to /cmd_vel node of type geometry_msgs/Twist.
+    """
+
     global index
     global angle
-    global dist_target
-    global list_of_points
+    #global dist_target
+    global list_of_way_points
     global speed
 
     x_pos =  msg.pose.position.x
@@ -72,60 +42,39 @@ def scan_received(msg):
 
     angle_current = euler[2]*180/math.pi
 
-    if index >= len(list_of_points):
+    if index >= len(list_of_way_points):
         angle = 0
         speed = 0
+
     else:
         speed = .05
-        delta_y = list_of_points[index][1] - y_pos
-        delta_x = list_of_points[index][0] - x_pos
-        dist_target = math.sqrt((list_of_points[index][0]-x_pos)**2+(list_of_points[index][1]-y_pos)**2)
+        delta_y = list_of_way_points[index][1] - y_pos
+        delta_x = list_of_way_points[index][0] - x_pos
+        dist_target = math.sqrt((list_of_way_points[index][0]-x_pos) ** 2 + (list_of_way_points[index][1]-y_pos) ** 2)
         angle_target = math.atan2(delta_y,delta_x)*180/math.pi
+
         if angle_target<0:
             angle_target = 360 + angle_target
+
         if angle_current<0:
             angle_current = 360 + angle_current
+
         angle = angle_target - angle_current
-        if 0<= angle_target <= 180 and 180<=angle_current<= 360:
+
+        if 0 <= angle_target <= 180 and 180 <= angle_current <= 360:
             angle = angle_target + 360 - angle_current
-        if 0<= angle_current <= 180 and 180<=angle_target<= 360:
+
+        if 0 <= angle_current <= 180 and 180 <= angle_target <= 360:
             angle = 360 - angle_current - angle_target
 
         if dist_target < 0.5:
             index += 1
 
-    #print 'index'
-   # print index
-
-
-    #print dist_target
-    #print 'tar'
-    #print angle_target
-   # print 'curr'
-   # print angle_current
-
-
-
-   # print 'ang'
-   # print angle
-
     return dist_target, angle
 
- 
-def follow_waypoint(pub):
+def get_to_waypoint(pub):
     r = rospy.Rate(10)
     while not (rospy.is_shutdown()):
-        #print angle
-
-
-        #print quaternion
-        #euler = tf.transformations.euler_from_quaternion(quaternion)
-        #roll = euler[0]
-        #pitch = euler[1]
-        #yaw = euler[2]
-
-        #print math.atan2(5,-5)*180/math.pi
-
         velocity_msg = Twist(Vector3(speed,0.0,0.0),Vector3(0.0,0.0,0.003*angle))
         pub.publish(velocity_msg)
         r.sleep()
@@ -134,6 +83,6 @@ if __name__ == '__main__':
     try:
         rospy.init_node('robot_direct', anonymous=True)
         pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
-        sub = rospy.Subscriber('particle', PoseStamped, scan_received)
-        follow_waypoint(pub)
+        sub = rospy.Subscriber('particle', PoseStamped, calculate_heading_speed)
+        get_to_waypoint(pub)
     except rospy.ROSInterruptException: pass
