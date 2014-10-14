9bf8fcdacb57656ba279e2ec424d707e09b72bf8
Halie Murray-Davis <hmurraydavis@yahoo.com>
2014-10-14 00:41:48 -0400

    head banging

--- path_planning/scripts/parse_map.py
+++ path_planning/scripts/parse_map.py
@@ -88,10 +88,11 @@ def dijkstraR(space, currentNode, goal, nodesVisited, node_dists, node_progressi
             xnode,ynode=node
             xdesired_paths.append(xnode)
             ydesired_paths.append(ynode)
+        xdesired_paths=Int16MultiArray(xdesired_paths)
         print 'x desired nodes: ',xdesired_paths
         print 'ydesired_paths: ', ydesired_paths
 
-#        pub.publish(desired_path)
+        #pub.publish(xdesired_paths)
         return desired_path
 
     for child in space[currentNode]:
